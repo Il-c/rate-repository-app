49b7f3432fca229864fe5ad14cbe9e4fbafd7024
Ilari Lehtinen <ilari.m.lehtinen@gmail.com>
2020-10-08 16:03:23 +0300

    osa1 valmis

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('kommentti');
   return (
     <View style={styles.container}>
       <Text>Miten  tämä muuttuu?</Text>
